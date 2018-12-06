86e3da81f6ed4cb9ed49341be57b576273d9fc71
Kelvin Lee <kiyolee@gmail.com>
2018-12-06 22:21:55 +1100

    Fix testing statically linked binaries.

--- ms/test.bat
+++ ms/test.bat
@@ -8,77 +8,77 @@ set OPENSSL_CONF=..\apps\openssl.cnf
 rem run this from inside the bin directory
 
 echo rsa_test
-rsa_test%BIN_SUFFIX%
+rsa_test
 if errorlevel 1 goto done
 
 echo destest
-destest%BIN_SUFFIX%
+destest
 if errorlevel 1 goto done
 
 echo ideatest
-ideatest%BIN_SUFFIX%
+ideatest
 if errorlevel 1 goto done
 
 echo bftest
-bftest%BIN_SUFFIX%
+bftest
 if errorlevel 1 goto done
 
 echo shatest
-shatest%BIN_SUFFIX%
+shatest
 if errorlevel 1 goto done
 
 echo sha1test
-sha1test%BIN_SUFFIX%
+sha1test
 if errorlevel 1 goto done
 
 echo md5test
-md5test%BIN_SUFFIX%
+md5test
 if errorlevel 1 goto done
 
 echo rc2test
-rc2test%BIN_SUFFIX%
+rc2test
 if errorlevel 1 goto done
 
 echo rc4test
-rc4test%BIN_SUFFIX%
+rc4test
 if errorlevel 1 goto done
 
 echo randtest
-randtest%BIN_SUFFIX%
+randtest
 if errorlevel 1 goto done
 
 echo dhtest
-dhtest%BIN_SUFFIX%
+dhtest
 if errorlevel 1 goto done
 
 echo exptest
-exptest%BIN_SUFFIX%
+exptest
 if errorlevel 1 goto done
 
 echo dsatest
-dsatest%BIN_SUFFIX%
+dsatest
 if errorlevel 1 goto done
 
 echo ectest
-ectest%BIN_SUFFIX%
+ectest
 if errorlevel 1 goto done
 
 echo testenc
-call %test%\testenc openssl%BIN_SUFFIX%
+call %test%\testenc openssl
 if errorlevel 1 goto done
 
 echo testpem
-call %test%\testpem openssl%BIN_SUFFIX%
+call %test%\testpem openssl
 if errorlevel 1 goto done
 
 echo testss
-call %test%\testss openssl%BIN_SUFFIX%
+call %test%\testss openssl
 if errorlevel 1 goto done
 
-set SSL_TEST=ssltest%BIN_SUFFIX% -key keyU.ss -cert certU.ss -c_key keyU.ss -c_cert certU.ss -CAfile certCA.ss
+set SSL_TEST=ssltest -key keyU.ss -cert certU.ss -c_key keyU.ss -c_cert certU.ss -CAfile certCA.ss
 
 echo test sslv2
-ssltest%BIN_SUFFIX% -ssl2
+ssltest -ssl2
 if errorlevel 1 goto done
 
 echo test sslv2 with server authentication
@@ -94,7 +94,7 @@ echo test sslv2 with both client and server authentication
 if errorlevel 1 goto done
 
 echo test sslv3
-ssltest%BIN_SUFFIX% -ssl3
+ssltest -ssl3
 if errorlevel 1 goto done
 
 echo test sslv3 with server authentication
@@ -110,7 +110,7 @@ echo test sslv3 with both client and server authentication
 if errorlevel 1 goto done
 
 echo test sslv2/sslv3
-ssltest%BIN_SUFFIX%
+ssltest
 if errorlevel 1 goto done
 
 echo test sslv2/sslv3 with server authentication
@@ -126,11 +126,11 @@ echo test sslv2/sslv3 with both client and server authentication
 if errorlevel 1 goto done
 
 echo test sslv2 via BIO pair
-ssltest%BIN_SUFFIX% -bio_pair -ssl2
+ssltest -bio_pair -ssl2
 if errorlevel 1 goto done
 
 echo test sslv2/sslv3 with 1024 bit DHE via BIO pair
-ssltest%BIN_SUFFIX% -bio_pair -dhe1024dsa -v
+ssltest -bio_pair -dhe1024dsa -v
 if errorlevel 1 goto done
 
 echo test sslv2 with server authentication via BIO pair
@@ -146,7 +146,7 @@ echo test sslv2 with both client and server authentication via BIO pair
 if errorlevel 1 goto done
 
 echo test sslv3 via BIO pair
-ssltest%BIN_SUFFIX% -bio_pair -ssl3
+ssltest -bio_pair -ssl3
 if errorlevel 1 goto done
 
 echo test sslv3 with server authentication via BIO pair
@@ -162,7 +162,7 @@ echo test sslv3 with both client and server authentication via BIO pair
 if errorlevel 1 goto done
 
 echo test sslv2/sslv3 via BIO pair
-ssltest%BIN_SUFFIX% -bio_pair
+ssltest -bio_pair
 if errorlevel 1 goto done
 
 echo test sslv2/sslv3 with server authentication
--- ms/test_one.cmd
+++ ms/test_one.cmd
@@ -13,7 +13,7 @@ set BIN_DIR=%1
 
 :start0
 if .%BIN_SUFFIX%. == .. goto :nosuffix
-echo TESTING: %BIN_DIR% (BIN_SUFFIX:%BIN_SUFFIX%)
+echo TESTING: %BIN_DIR% (TESTOPT:%BIN_SUFFIX%)
 goto :start
 :nosuffix
 echo TESTING: %BIN_DIR%
@@ -23,19 +23,27 @@ echo.
 
 if not exist %BIN_DIR%\openssl%BIN_SUFFIX%.exe goto :noexe
 
-pushd %MY_ROOT%\ms
-
-path %BIN_DIR%;%PATH%
-
 echo VERSION-INFO:
-openssl%BIN_SUFFIX% version -a
+%BIN_DIR%\openssl%BIN_SUFFIX% version -a
 
 if .%SKIP_TEST%. == .YES. goto :skiptest
 echo.
+
+if .%BIN_SUFFIX%. == .-static. goto :usestat
+path %BIN_DIR%;%PATH%
+goto :dotest
+:usestat
+path %BIN_DIR%\static;%PATH%
+
+:dotest
+
+pushd %MY_ROOT%\ms
+
 call test.bat
-:skiptest
 
 popd
+
+:skiptest
 
 goto :done
 
